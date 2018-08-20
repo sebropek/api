eebc8854e91eb921aa36e244ac3f06d6a683f329
blef <seboblef@gmail.com>
2018-08-20 23:56:49 +0200

    second commit

---
+++ api.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import json
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date, timedelta
+
+app = Flask(__name__)
+api = Api(app,default_mediatype='application/json')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////root/app/api-db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class HelloWorld(db.Model):
+    name = db.Column(db.String(64), primary_key=True)
+    dateOfBirth = db.Column(db.Date)
+    def __repr__(self):
+        return '<HelloWorld {}>'.format(self.name) 
+
+class Api(Resource):
+    def get(self, user_name):
+	messages = {}
+	user = HelloWorld.query.filter_by(name=user_name).first_or_404()
+	user_date=user.dateOfBirth
+	if (user_date - date.today()).days == 0:
+		messages[user_name]={'message': 'Hello ' + user_name + '! Happy birthday! ' }
+	elif ( (user_date - date.today()).days >0 and (user_date - date.today()).days <= 5 ):
+		messages[user_name]={'message': 'Hello ' + user_name + '! Your birthday is in 5 days ' }
+	else:
+		messages[user_name]=''
+	return json.dumps(messages[user_name]), 200
+
+    def put(self, user_name):
+	names = {}
+	names[user_name] = request.get_json(force=True)
+	hw = HelloWorld(name=user_name, dateOfBirth=datetime.strptime(names[user_name]['dateOfBirth'], '%Y-%m-%d' ) )
+	try:
+       	  db.session.add(hw)
+	  db.session.commit()
+          return '', 204
+	except:
+	  db.session.rollback() 
+	  return 'user already exists', 500
+ 	
+api.add_resource(Api, '/Hello/<string:user_name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ db.sql
@@ -0,0 +1,5 @@
+drop table Hello_World;
+create table Hello_World(name varchar(64) PRIMARY KEY, dateOfBirth date);
+insert into Hello_World values('JohnSnow', '2001-01-01');
+insert into Hello_World values('JohnBean', '2002-02-01');
+
