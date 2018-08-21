eee38f35051f4fa80ce56c7bc0a94a43d4ae940e
sebropek <sebropek@gmail.com>
2018-08-21 05:17:45 +0200

    sqlite 2 mysql migration

--- api.py
+++ api.py
@@ -6,7 +6,8 @@ from datetime import datetime, date, timedelta
 
 app = Flask(__name__)
 api = Api(app,default_mediatype='application/json')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////opt/app/api-db.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////opt/app/api-db.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://api:api@localhost:3306/api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-create table Hello_World(name varchar(64) PRIMARY KEY, dateOfBirth date);
-insert into Hello_World values('JohnSnow', '2001-01-01');
-insert into Hello_World values('JohnBean', '2002-02-01');
+create table hello_world(name varchar(64) PRIMARY KEY, dateOfBirth date);
+insert into hello_world values('JohnSnow', '2001-01-01');
+insert into hello_world values('JohnBean', '2002-02-01');
 
